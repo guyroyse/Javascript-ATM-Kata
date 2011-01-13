7dd152fc30a66a47b09855aa1a45b79af6bf9d01
Guy Royse <guy@guyroyse.com>
2011-01-13 15:16:53 -0500

    Implemented 4 or 5 stories

--- src/ATM.js
+++ src/ATM.js
@@ -1,13 +1,46 @@
-var ATM = (function() {
-    var ATM = function() {}
-    ATM.prototype = {
-        withdraw: function() {
-
-        }
-    }
-    return {
-        create: function() {
-            return new ATM();
-        }
-    }
+var atm = (function() {
+	var validatePin = function(pin) {
+		return (pin == '1234');
+	};
+	
+	var validateAccountNumber = function(accountNumber) {
+		return (accountNumber == '1234567890');
+	};
+	
+	var validateCredentials = function(accountNumber, pin) {
+		return validatePin(pin) && validateAccountNumber(accountNumber);
+	}
+	
+	return {
+		withdraw: function(amount, accountNumber, pin) {
+			if (!validateCredentials(accountNumber, pin)) amount = 0;
+			if (amount > this.balance) amount = 0;
+			return amount;
+		}
+	};
 })();
+
+var view = {
+	getAmount: function() {
+		return $('input#amount').val();
+	},
+	getPin: function() {
+		return $('input#PIN').val();
+	},
+	getAccountNumber: function() {
+		return $('input#accountNumber').val();
+	},
+	setCashDrawer: function(amount) {
+		$('#cashDrawer').val(amount);		
+	},
+	setBalance: function(amount) {
+		$('#accountBalance').val(amount);		
+	}
+};
+
+$(function() {
+	$('input#withdraw').click(function() {
+		var amountWithdrawn = atm.withdraw(view.getAmount(), view.getAccountNumber(), view.getPin());
+		view.setCashDrawer(amountWithdrawn);
+	});
+});
--- test/ATMStories.js
+++ test/ATMStories.js
@@ -2,41 +2,41 @@ describe("ATM Story Tests", function(){
     beforeEach(function() {
         // Put however you reset everything in here.
         // This should reset Judy's account balance too.
-        
+    	atm.balance = 100;
     });
     it("Allows Judy to withdraw money when she has a balance available", function() {
         $('input#accountNumber').val('1234567890');
         $('input#PIN').val('1234');
         $('input#amount').val(10);
         $('input#withdraw').click();
-        expect($('#cashDrawer').val()).toEqual(10);
+        expect($('#cashDrawer').val()).toEqual('10');
     });
     it("Prevents Judy from withdrawing money when she doesn't have it available", function() {
         $('input#accountNumber').val('1234567890');
         $('input#PIN').val('1234');
         $('input#amount').val(110);
         $('input#withdraw').click();
-        expect($('#cashDrawer').val()).toEqual(0);
+        expect($('#cashDrawer').val()).toEqual('0');
     });
     it("Prevents Judy from withdrawing money when she puts in an incorrect pin", function() {
         $('input#accountNumber').val('1234567890');
         $('input#PIN').val('5678');
         $('input#amount').val(25);
         $('input#withdraw').click();
-        expect($('#cashDrawer').val()).toEqual(0);
+        expect($('#cashDrawer').val()).toEqual('0');
  });
     it("Prevents Judy from withdrawing money when she puts in an incorrect account number", function() {
         $('input#accountNumber').val('0987654321');
         $('input#PIN').val('1234');
-        $('input#amount').val(25);
+        $('input#amount').val('25');
         $('input#withdraw').click();
-        expect($('#cashDrawer').val()).toEqual(0);
+        expect($('#cashDrawer').val()).toEqual('0');
    });
     it("Updates Judys balance when she withdraws money", function() {
         $('input#accountNumber').val('0987654321');
         $('input#PIN').val('1234');
         $('input#amount').val(10);
         $('input#withdraw').click();
-        expect($('#accountBalance').text()).toEqual(90);
+        expect($('#accountBalance').text()).toEqual('90');
     });
 });
